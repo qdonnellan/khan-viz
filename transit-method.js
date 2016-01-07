31a1e531011eaa2b46df075a5bdb9c87c33926db
Quentin Donnellan <quentin@spideroak.com>
2016-01-07 07:49:24 -0600

    saving progress on the light curve; getting a bit tricky now...

--- transit-method.js
+++ transit-method.js
@@ -1,19 +1,19 @@
-
 var Field = function() {
-    this.draw = function() {
-        // The background.
-        
-    
-        // Dividing line between star and light curve.
-        stroke(50, 50, 50);
-        strokeWeight(1);
-        line(0,200,400,200);
-        
-        // Text
+    this.draw_once = function() {
+        background(0,0,0);
+        strokeWeight(2);
         fill(255, 255, 255);
         text('Light Curve', 10, 220);
         fill(153, 153, 153);
-        text('Measures the amount of light reaching us as we look at the star', 10, 235);
+        text('Amount of light reaching us as we look at the star', 10, 235);
+    };
+
+    this.draw = function() {
+        // The background.
+        fill(0, 0, 0);
+        stroke(0,0,0);
+        strokeWeight(0);
+        rect(0,0,400,200);
     };
 };
 
@@ -37,7 +37,7 @@ var Planet = function() {
     
     self.x = 50;
     self.y = 100;
-    self.size = 20;
+    self.size = 40;
     
     self.draw = function() {
         noStroke();
@@ -82,6 +82,33 @@ var LightCurve = function() {
     var self = this;
     self.y = 250;
     
+    self.plot_point = function(S, P, x) {
+        var max_light = Math.PI*pow(S.size/2, 2);
+        var percent_light;
+        if (x % 360 > 180) {
+            var percent_light = 1;
+        } else {
+            var light_blocked = self.area_of_intersection(S, P);
+            var percent_light = (max_light + light_blocked) / max_light;
+        }
+        stroke(171, 7, 171);
+        strokeWeight(1);
+        point(x/4, percent_light*self.y);
+    };
+    
+    self.get_y_value = function(S,P, x) {
+        // For a given "x" value, return a "y" value.
+        var max_light = Math.PI*pow(S.size/2, 2);
+        var percent_light;
+        if (x % 360 > 180) {
+            var percent_light = 1;
+        } else {
+            var light_blocked = self.area_of_intersection(S, P);
+            var percent_light = (max_light + light_blocked) / max_light;
+        }
+        return percent_light*self.y;
+    };
+    
     self.area_of_intersection = function(S, P) {
         // Find the total area of intersection between a Star (S) and a Planet (P).
         var Rs = S.size/2;
@@ -90,15 +117,13 @@ var LightCurve = function() {
         // The distance between the centers.
         var d = sqrt(sq(S.x - P.x) + sq(S.y - P.y));
         
-        if (Rs+Rp >= d) {
+        if (Rs+Rp < d) {
             return 0;
         } else if (d + Rp < Rs) {
             return sq(P.size)*Math.PI;
         } else {
             angleMode = 'radians';
             var arg1 = sq(Rp)*acos((sq(d)+sq(Rp)-sq(Rs))/(2*d*Rp));
-            
-            text(arg1, 10, 20);
             var arg2 = sq(Rs)*acos((sq(d)+sq(Rs)-sq(Rp))/(2*d*Rs));
             var arg3 = sqrt((Rp+Rs-d)*(d+Rp-Rs)*(d+Rs-Rp)*(d+Rp+Rs))/2;
             angleMode = 'degrees';
@@ -107,26 +132,21 @@ var LightCurve = function() {
         
     };
     
-    self.plot_point = function(S, P, x) {
-        var max_light = Math.PI*pow(S.size/2, 2);
-        var light_blocked = self.area_of_intersection(S, P);
-        var percent_light = (max_light - light_blocked) / max_light;
-        stroke(171, 7, 171);
-        strokeWeight(10);
-        point(x, percent_light*self.y);
-    };
     
 
 };
 
 
 var field = new Field();
+field.draw_once();
+
 var star = new Star(); 
 var planet = new Planet();
 var light_curve = new LightCurve();
 
-frameRate(100);
+frameRate(1000);
 var step = 0 ;
+
 
 draw = function() {
     planet.x = (200-planet.size/2)*sin(step - 90) + 200;
@@ -143,6 +163,5 @@ draw = function() {
     
     light_curve.plot_point(star, planet, step);
     
-    step += 1;
-};
-
+    step += 0.1;
+}; 
